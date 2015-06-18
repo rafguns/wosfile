5ee65a23ff8cb392a83bb35a4e2c48712ce0daf7
Raf Guns <rafguns@gmail.com>
2015-06-18 13:17:47 +0200

    Add project files (setup, Travis etc.)

---
+++ setup.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+from setuptools import setup
+
+long_description = open('README.md').read()
+
+setup(
+    name='wos',
+    version='0.1',
+    url='http://github.com/rafguns/wos/',
+    license='New BSD License',
+    author='Raf Guns',
+    tests_require=['nose'],
+    author_email='raf.guns@uantwerpen.be',
+    description='Handle Thomson Reuters Web of Science™ export files',
+    long_description=long_description,
+    packages=['gefura'],
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ]
+)
